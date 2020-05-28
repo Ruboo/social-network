74e884bc0a13347d070726981f7146b379035011
Rubo <rubominas@gmail.com>
2020-05-28 18:27:35 +0400

    added navbar icons

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -3,7 +3,6 @@ import Header from './Header';
 import * as axios from 'axios';
 import {connect} from 'react-redux';
 import { getAuthUserData } from '../../redux/authReducer';
-import { authAPI } from '../../api/api';
 
 
 class HeaderContainer extends React.Component {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,47 @@
 import React from 'react';
-import classes from'./Navbar.module.css';
+import classes from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import SettingsIcon from '@material-ui/icons/Settings';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
+import ChatIcon from '@material-ui/icons/Chat'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';;
 
 const Navbar = () => {
-    return (
-        <nav className={classes.navbar}>
-        <div className = {classes.item}>
-          <NavLink to = "/profile" activeClassName = {classes.active}>Profile</NavLink>
-        </div>
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.item}>
+        <AccountCircleIcon />
+        <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
+      </div>
 
-        <div className = {classes.item}>
+      <div className={classes.item}>
+        <ChatIcon />
+        <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
+      </div>
 
-          <NavLink to = "/dialogs" activeClassName = {classes.active}>Messages</NavLink>
-        </div>
+      <div className={classes.item}>
+        <AnnouncementIcon />
+        <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
+      </div>
 
-        <div className = {classes.item}>
-          <NavLink to= "/news" activeClassName = {classes.active}>News</NavLink>
-        </div>
+      <div className={classes.item}>
+        <LibraryMusicIcon />
+        <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
+      </div>
 
-        <div className = {classes.item}>
-        <NavLink to= "/music" activeClassName = {classes.active}>Music</NavLink>  
-              </div>
+      <div className={classes.item}>
+        <PeopleAltIcon />
+        <NavLink to="/users" activeClassName={classes.active}>Users</NavLink>
+      </div>
+      <div className={classes.item}>
+        <SettingsIcon />
+        <NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink>
+      </div>
 
-         <div className = {classes.item}>
-          <NavLink to= "/users" activeClassName = {classes.active}>Users</NavLink>
-        </div>
-        <div className = {classes.item}>
-        <NavLink to= "/settings" activeClassName = {classes.active}>Settings</NavLink>    
-            </div>
-
-      </nav>
-    )
+    </nav>
+  )
 }
 
 export default Navbar;
